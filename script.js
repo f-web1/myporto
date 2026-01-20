c64d646a1c289e88c23c6e14149607173f787e2b
F-Dev23 <arsanafahri@gmail.com>
2026-01-20 20:34:45 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+/* --- TOGGLE ICON NAVBAR --- */
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-x');
+    navbar.classList.toggle('active');
+};
+
+/* --- SCROLL SECTIONS ACTIVE LINK --- */
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    /* --- STICKY NAVBAR --- */
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /* --- REMOVE TOGGLE ICON AND NAVBAR WHEN CLICK LINK --- */
+    menuIcon.classList.remove('fa-x');
+    navbar.classList.remove('active');
+};
+
+/* --- SCROLL REVEAL (Animasi Muncul) --- */
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+/* --- TYPED JS (Efek Ketik) --- */
+// Saya set hanya 'Web Developer' saja sesuai permintaan
+const typed = new Typed('.multiple-text', {
+    strings: ['Web Developer'], // Hanya satu teks
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true // Tetap loop agar efek ketiknya berulang (keren), atau set false jika ingin diam
+});
